33cc692185a2c2f804615d36eade212ce7448cd2
papeloto <vn4varro@gmail.com>
2020-03-13 21:17:43 +0100

    feat: set button grid overflow

--- src/components/box.js
+++ src/components/box.js
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 
 const Box = styled.a`
   box-sizing: border-box;
+  display: block;
   width: 100%;
   padding: 16px 12px;
   box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.03);
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,17 +1,31 @@
 import { h } from 'preact';
+import styled from 'styled-components';
 import Content from '../components/content';
 import Button from '../components/button';
 import AddButton from '../components/add-button';
 import { useGroups } from '../contexts/groups-context';
+
+const ButtonGrid = styled.div`
+	max-height: 200px;
+	display: grid;
+	gap: 8px;
+	overflow-y: scroll;
+
+	&::-webkit-scrollbar {
+		display: none;
+	}
+`;
 
 const Home = () => {
 	const { groups } = useGroups();
 
 	return (
 		<Content>
-			{groups.map(group => (
-				<Button key={group.id}>{group.name}</Button>
-			))}
+			<ButtonGrid>
+				{groups.map(group => (
+					<Button key={group.id}>{group.name}</Button>
+				))}
+			</ButtonGrid>
 			<AddButton to="/create" />
 		</Content>
 	);
